52bff1587bec05ad112283a3fa9e65ac7cc4a1da
Zihui Yang <yangahq@bc.edu>
2021-09-30 16:23:11 -0400

    colors-and-axes

--- script.js
+++ script.js
@@ -9,10 +9,16 @@ async function main(){
     const url = "wealth-health-2014.csv";
     ori_data = await loadData(url);
 
+    const margin = ({top: 30, right: 30, bottom: 30, left: 30})
+    const width = 750 - margin.left - margin.right;
+    const height = 600 - margin.top - margin.bottom;
+
     const svg = d3.select('.chart')
                     .append('svg') 
-                    .attr("width", 1100)
-                    .attr("height", 900)
+                    .attr("width", width + margin.left + margin.right)
+                    .attr("height", height + margin.top + margin.bottom)
+	                .append("g")
+                    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
     const lifeArr = [];
     const incomeArr = [];
@@ -24,11 +30,36 @@ async function main(){
 
     var xScale = d3.scaleLinear()
                     .domain([xRange[0], xRange[1]])
-                    .range([0, 1100])
+                    .range([0, 690])
 
     var yScale = d3.scaleLinear()
                     .domain([yRange[1], yRange[0]])
-                    .range([0, 900])
+                    .range([0, 540])    
+
+    const xAxis = d3.axisBottom()
+    .ticks(5, "s")
+	.scale(xScale);
+
+    const yAxis = d3.axisLeft()
+    .ticks(6, "s")
+	.scale(yScale);
+
+    svg.append("g")
+	.attr("class", "axis x-axis")
+    .attr("transform", `translate(0, ${height})`)
+	.call(xAxis);
+    
+    svg.append("g")
+	.attr("class", "axis y-axis")
+    .attr("transform", `translate(0, ${width} - 20)`)
+	.call(yAxis);
+
+    let regionArr = [];
+    ori_data.forEach(a => regionArr.push(a.Region));
+
+    let uniqueRegion = [...new Set(regionArr)];
+
+    const color = d3.scaleOrdinal(d3.schemeTableau10).domain(regionArr);
 
     svg.selectAll("scatter-plot")
         .data(ori_data)
@@ -42,6 +73,41 @@ async function main(){
         .attr('fill', "rgb(76, 162, 219)")
         .attr('opacity', "0.7")
         .attr('stroke', "black")
+        .attr("fill", function(d){return color(d.Region)});
+    
+    svg.append("text")
+        .attr('x', 635)
+        .attr('y', 535)
+        .text("Income")
+
+    svg.append("text")
+        .attr('x', 10)
+        .attr('y', 0)
+        .attr('class', 'vertical')
+        .text("Life Expectancy")
+
+    svg.selectAll("legend")
+        .data(uniqueRegion)
+        .enter()
+        .append("g")
+        .append("rect")
+        .attr("x", 500)
+        .attr("y", function(d,i){ return 300 + i*25.5})
+        .attr("width", 20)
+        .attr("height", 20)
+        .style("fill", function(d){
+            return color(d)
+        })
+
+    svg.selectAll("legendText")
+        .data(uniqueRegion)
+        .enter()
+        .append("text")
+        .attr("x", 523)
+        .attr("y", function(d,i){ return 309 + i*26})
+        .text(function(d){ return d})
+        .attr("text-anchor", "left")
+        .style("alignment-baseline", "middle")
 }
 
 main();
